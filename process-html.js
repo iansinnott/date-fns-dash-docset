f9b8a7ebf36f0d21641c4df360fb6ed519c33b22
Ian Sinnott <ian@iansinnott.com>
2017-08-17 14:12:31 -0700

    Add custom styles and other processing

--- process-html.js
+++ process-html.js
@@ -4,16 +4,52 @@ const fs = require('fs');
 const path = require('path');
 const { map, pipe, trim } = require('ramda');
 const cheerio = require('cheerio');
+const stripIndent = require('strip-indent');
 
 const { fromReadableStream } = require('./utils.js');
+
+const heredoc = pipe(trim, stripIndent);
 
 const processHTML = (html) => {
   const $ = cheerio.load(html);
 
-  const $links = $('.docs_finder-list > a')
-    .each((index, el) => {
-      console.log('sutff');
+  // Add custom styles
+  $('head').append(heredoc(`
+    <style>
+      .ui .docs-finder {
+        display: none;
+      }
+      .ui .docs {
+        padding-top: 0;
+      }
+      .ui .docs_nav_bar {
+        display: none;
+      }
+      .ui .docs-content {
+        padding-left: 0;
+      }
+      .ui .jsdoc_usage-options .jsdoc_usage-option:not(:first-child) {
+        display: none;
+      }
+    </style>
+  `));
+
+  // Remove non-local scripts. These docs are meant for offline use so let's not
+  // pull in any remote scripts
+  $('script[type="text/javascript"]')
+    .filter((i, el) => {
+      const src = $(el).attr('src');
+      return !/^\/assets\/js/.test(src);
     })
+    .remove();
+
+  // Remove unecessary meta tags
+  $('meta')
+    .filter((i, el) => {
+      const name = $(el).attr('name') || $(el).attr('property');
+      return /^(twitter|og):/.test(name);
+    })
+    .remove();
 
   return $.html();
 };
