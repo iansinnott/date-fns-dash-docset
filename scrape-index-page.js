32f881651a200425dbe3da4ef1bcbb18421c8993
Jari Zwarts <jari@oberon.nl>
2020-01-01 16:09:50 +0100

    Now works fully with dfns 2.0
    - Use latest version of dashing because 3.0 was crashing
    - Strip navigation from pages to prevent them from showing up in Dash
    - Use page headers as the selector instead of nav items.

--- scrape-index-page.js
+++ scrape-index-page.js
@@ -3,7 +3,7 @@
 const { loadURL, isDebugging } = require('./utils.js');
 
 const main = async () => {
-  const result = await loadURL('https://date-fns.org/docs/Getting-Started');
+  const result = await loadURL('https://date-fns.org/docs/Getting-Started', true);
 
   if (isDebugging()) {
     console.log('Fetched page source:', result);
